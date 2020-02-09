b56259c01d7e74ba250a03d42d50ca0197c5fd7d
thiagofd <thiago.fdornelles@gmail.com>
2020-02-08 21:07:53 -0300

    define routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 import Dashboard from '../pages/Dashboard';
+import Details from '../pages/Details';
+import New from '../pages/New';
+import Edit from '../pages/Edit';
 import Profile from '../pages/Profile';
 
 export default function Routes() {
@@ -13,8 +18,13 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/details" component={Details} isPrivate />
+      <Route path="/new" component={New} isPrivate />
+      <Route path="/edit" component={Edit} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+
+      <Route path="/" component={() => <h1>404 not found</h1>} />
     </Switch>
   );
 }
