2438d5456fb2fba2b8cbfc052fdc4d005d2b3aa4
thiagofd <thiago.fdornelles@gmail.com>
2020-02-09 13:15:23 -0300

    config root import

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@ import { Switch } from 'react-router-dom';
 
 import Route from './Route';
 
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
 
-import Dashboard from '../pages/Dashboard';
-import Details from '../pages/Details';
-import New from '../pages/New';
-import Edit from '../pages/Edit';
-import Profile from '../pages/Profile';
+import Dashboard from '~/pages/Dashboard';
+import Details from '~/pages/Details';
+import New from '~/pages/New';
+import Edit from '~/pages/Edit';
+import Profile from '~/pages/Profile';
 
 export default function Routes() {
   return (
